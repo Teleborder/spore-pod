4abdfb3bd9890eaf24a643a6971f7593dd82adae
Trey Griffith <trey.griffith@gmail.com>
2015-05-14 20:57:15 -0700

    use https only in production

--- middleware.js
+++ middleware.js
@@ -4,6 +4,17 @@ var path = require('path'),
     cookieParser = require('cookie-parser');
 
 function middleware(app) {
+
+  // Only allow secure requests in production
+  app.use(function (req, res, next) {
+    // heroku sets `x-forwarded-proto` when proxying
+    if(app.get('env') === 'production' && !req.secure && req.headers['x-forwarded-proto'] !== 'https') {
+      res.redirect('https://' + req.hostname + ':' + app.address().port + req.originalUrl);
+    }
+
+    next();
+  });
+
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded());
